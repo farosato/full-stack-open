d5ad8968fd0bad2cef897470bd6333138c381df9
Fabio Rosato <far@bendingspoons.com>
2022-07-05 19:34:31 +0200

    feat: add solution to ex 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Display = ({ text, value }) => <p>{`${text} ${value}`}</p>;
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      <h2>statistics</h2>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+    </div>
+  );
+};
+
+export default App;
